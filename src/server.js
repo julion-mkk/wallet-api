315c42ea9819a248fa431dc6a002d3b19de5de66
Min Khant Kyaw <mkk51198@gmail.com>
2025-11-19 19:21:03 +0900

    cron.js added

--- src/server.js
+++ src/server.js
@@ -3,10 +3,13 @@ import dotenv from "dotenv";
 import {initDB} from "./config/db.js";
 import rateLimiter from "./middleware/reateLimiter.js";
 import transactionsRoute from "./routes/transactionsRoute.js";
-
+import job from "./config/cron.js";
 dotenv.config();
 
 const app = express();
+if(process.env.NODE_ENV !== "production") {
+    job.start();
+}
 app.use(rateLimiter);
 app.use(express.json());
 
@@ -19,7 +22,9 @@ app.use(express.json());
 app.use("/api/transactions", transactionsRoute);
 
 const PORT = process.env.PORT || 5001;
-
+app.get('/api/health', (req, res)=> {
+    res.status(200).json({status: 'OK'});
+})
 initDB().then(()=> {
     app.listen(PORT, ()=> {
         console.log("Server is running on port", PORT);
